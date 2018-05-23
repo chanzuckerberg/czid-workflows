93895b58212d411b07287d9ebb6e3f35c3935299
yunfangjuan <yunfang@chanzuckerberg.com>
2018-05-22 17:29:24 -0700

    python

--- short-read-mngs/idseq-dag/idseq_dag/main.py
+++ short-read-mngs/idseq-dag/idseq_dag/main.py
@@ -1,3 +1,16 @@
+import argparse
+import json
+import sys
+
+def validate_dag_json(json_file):
+    return True
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dag_json', help='pipeline dag in json file format')
+    args = parser.parse_args()
+    dag_json_file = args.dag_json
+    if not validate_dag_json(dag_json_file):
+        parser.print_help()
+        sys.exit(1)
     print("idseq dag is awesome")
 
