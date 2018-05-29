f91508776870db4f28eddca0e3ea8283b7b1308a
yunfangjuan <yunfang@chanzuckerberg.com>
2018-05-29 16:01:44 -0700

    build up

--- short-read-mngs/idseq-dag/idseq_dag/main.py
+++ short-read-mngs/idseq-dag/idseq_dag/main.py
@@ -1,9 +1,46 @@
 import argparse
 import json
 import sys
+import os
+import traceback
+import idseq_dag.util.s3
 
 def validate_dag_json(json_file):
-    return True
+    '''
+    Validate the json format. see examples/*.json.
+    Required fields are:
+      "output_dir_s3": base results folder. a pipeline version number will be appended for real output folder.
+      "nodes": lists of files that are given or would be generated
+      "steps": steps that species actions to generate input and output
+      "head_nodes": input files that are given
+
+    '''
+    try:
+        dag = json.loads(open(json_file).read())
+        output_dir = dag["output_dir_s3"]
+        nodes = dag["nodes"]
+        steps = dag["steps"]
+        head_nodes = dag["head_nodes"]
+        for s in steps:
+            # validate each step in/out are valid nodes
+            output = nodes[s["out"]]
+            for inode in s["in"]:
+                input_n = nodes[inode]
+        for hn in head_nodes:
+            # validate the head nodes exist in s3
+            node_name = hn[0]
+            s3_path = hn[1]
+            for file_name in nodes[node_name]:
+                s3_file = os.path.join(s3_path, file_name)
+                if not idseq_dag.util.s3.check_s3_presence(s3_file):
+                    print("%s file doesn't exist" % s3_file)
+                    return False
+        # ALL Inputs Outputs VALIDATED
+        return True
+    except:
+        traceback.print_exc()
+        return False
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('dag_json', help='pipeline dag in json file format')
@@ -12,5 +49,5 @@ def main():
     if not validate_dag_json(dag_json_file):
         parser.print_help()
         sys.exit(1)
-    print("idseq dag is awesome")
+    print("everything is awesome. idseq dag is awesome")
 
---
+++ short-read-mngs/idseq-dag/idseq_dag/util/s3.py
@@ -0,0 +1,98 @@
+import os
+import threading
+import subprocess
+def check_s3_presence(s3_path):
+    ''' True if s3_path exists. False otherwise. '''
+    try:
+        o = subprocess.check_output("aws s3 ls %s" % s3_path, shell=True)
+        if o:
+            return True
+    except:
+        pass
+    return False
+
+def touch_s3_file(s3_file_path):
+    try:
+        subprocess.check_all("aws s3 cp --metadata '{\"touched\":\"now\"}' %s %s" % (s3_path, s3_path), shell=True)
+        return True
+    except:
+        return False
+
+def install_s3mi(installed={}, mutex=threading.RLock()):
+    #pylint: disable=dangerous-default-value
+    with mutex:
+        if installed:
+            return
+        try:
+            # This is typically a no-op.
+            execute_command(
+                "which s3mi || pip install git+git://github.com/chanzuckerberg/s3mi.git"
+            )
+            execute_command(
+                "s3mi tweak-vm || echo s3mi tweak-vm is impossible under docker"
+            )
+        finally:
+            installed['time'] = time.time()
+
+def fetch_from_s3(source,
+                  destination,
+                  auto_unzip,
+                  allow_s3mi=False,
+                  mutex=threading.RLock(),
+                  locks={}):  #pylint: disable=dangerous-default-value
+    with mutex:
+        if os.path.exists(destination):
+            if os.path.isdir(destination):
+                destination = os.path.join(destination,
+                                           os.path.basename(source))
+        unzip = auto_unzip and destination.endswith(".gz")
+        if unzip:
+            destination = destination[:-3]
+        abspath = os.path.abspath(destination)
+        if abspath not in locks:
+            locks[abspath] = threading.RLock()
+        destination_lock = locks[abspath]
+    with destination_lock:
+        if os.path.exists(destination):
+            # no need to fetch this file from s3,
+            # it has been just produced on this instance
+            return destination
+        try:
+            destdir = os.path.dirname(destination)
+            if destdir:
+                os.makedirs(destdir)
+        except OSError as e:
+            # It's okay if the parent directory already exists, but all other errors are fatal.
+            if e.errno != os.errno.EEXIST:
+                raise
+        with iostream:
+            try:
+                if allow_s3mi:
+                    try:
+                        install_s3mi()
+                    except:
+                        allow_s3mi = False
+                if unzip:
+                    pipe_filter = "| gzip -dc "
+                else:
+                    pipe_filter = ""
+                try:
+                    assert allow_s3mi
+                    execute_command(
+                        "s3mi cat {source} {pipe_filter} > {destination}".
+                        format(
+                            source=source,
+                            pipe_filter=pipe_filter,
+                            destination=destination))
+                except:
+                    execute_command(
+                        "aws s3 cp --quiet {source} - {pipe_filter} > {destination}".
+                        format(
+                            source=source,
+                            pipe_filter=pipe_filter,
+                            destination=destination))
+                return destination
+            except subprocess.CalledProcessError:
+                # Most likely the file doesn't exist in S3.
+                return None
+
