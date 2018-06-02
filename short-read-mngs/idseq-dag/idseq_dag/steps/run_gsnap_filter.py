209801d60d99ad86847add424e38da450a1c21ae
yunfangjuan <yunfang@chanzuckerberg.com>
2018-06-01 17:45:21 -0700

    dummy steps

--- short-read-mngs/idseq-dag/idseq_dag/steps/run_gsnap_filter.py
+++ short-read-mngs/idseq-dag/idseq_dag/steps/run_gsnap_filter.py
@@ -1,5 +1,6 @@
-
+import subprocess
 from idseq_dag.engine.pipeline_step import PipelineStep
 class PipelineStepRunGsnapFilter(PipelineStep):
     def run(self):
-        pass
+        for f in self.output_files_local():
+            subprocess.check_call("date > %s" % f, shell=True)
