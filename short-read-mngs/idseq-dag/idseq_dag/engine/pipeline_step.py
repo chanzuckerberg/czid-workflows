9d1fe154379a8aeff8ba33bbc126078bc0262965
yunfangjuan <yunfang@chanzuckerberg.com>
2018-06-01 17:33:36 -0700

    lint

--- short-read-mngs/idseq-dag/idseq_dag/engine/pipeline_step.py
+++ short-read-mngs/idseq-dag/idseq_dag/engine/pipeline_step.py
@@ -4,34 +4,38 @@ import os
 import threading
 import subprocess
 import time
+from abc import abstractmethod
 
 class PipelineStep:
+    ''' Each Pipeline Run Step i.e. run_star, run_bowtie, etc '''
     def __init__(self, input_files, output_files,
                  output_dir_local, output_dir_s3, ref_dir_local,
                  additional_files, additional_attributes):
-      ''' Set up all the input_files and output_files here '''
-      self.input_files = input_files # list of list files
-      self.output_files = output_files # s3 location
-      self.output_dir_local = output_dir_local
-      self.output_dir_s3 = output_dir_s3.rstrip('/')
-      self.ref_dir_local = ref_dir_local
-      self.additional_files = additional_files
-      self.additional_attributes = additional_attributes
+        ''' Set up all the input_files and output_files here '''
+        self.input_files = input_files # list of list files
+        self.output_files = output_files # s3 location
+        self.output_dir_local = output_dir_local
+        self.output_dir_s3 = output_dir_s3.rstrip('/')
+        self.ref_dir_local = ref_dir_local
+        self.additional_files = additional_files
+        self.additional_attributes = additional_attributes
 
-      self.started = False
-      self.finished = False # done running
-      self.all_done = False # done running and uploading
-      self.thread = None
-      self.input_files_local = []
+        self.started = False
+        self.finished = False # done running
+        self.all_done = False # done running and uploading
+        self.thread = None
+        self.input_files_local = []
 
     @abstractmethod
     def run(self):
-      ''' implement what is actually being run in this step '''
+        ''' implement what is actually being run in this step '''
 
     def output_files_local(self):
-        return [os.path.join(self.output_dir_local,f) for f in self.output_files]
+        ''' Get list of output files on local folder '''
+        return [os.path.join(self.output_dir_local, f) for f in self.output_files]
 
     def start_uploading_results(self):
+        ''' Tag the output files as done and upload to s3 '''
         for f in self.output_files_local():
             # Tag the done files
             done_file = self.done_file(f)
@@ -42,19 +46,23 @@ class PipelineStep:
 
     @staticmethod
     def done_file(filename):
+        ''' get the done file for a particular local file '''
         return "%s.done" % filename
 
-    def wait_for_file_existence(self, filename):
+    @staticmethod
+    def wait_for_file_existence(filename):
+        ''' wait until file exists '''
         while True:
             if os.path.exists(filename):
                 return
             time.sleep(5) # wait for 5 seconds to check again
 
     def wait_for_input_files(self):
+        ''' wait for all the input files to be avaiable and update input_files_local '''
         for fl in self.input_files:
             flist = []
             for f in fl:
-                local_file = os.path.join(self.output_dir_local,f)
+                local_file = os.path.join(self.output_dir_local, f)
                 self.wait_for_file_existence(local_file)
                 self.wait_for_file_existence(self.done_file(local_file))
                 flist.append(f)
@@ -62,12 +70,14 @@ class PipelineStep:
 
 
     def save_progress(self):
+        ''' save progress after step run '''
         # save stats
         # save other info
         # TO BE IMPLEMENTED
         pass
 
     def thread_run(self):
+        ''' Actually running the step '''
         #Timer.start()
         self.started = True
         self.wait_for_input_files()
@@ -79,8 +89,6 @@ class PipelineStep:
         self.all_done = True
 
     def start(self):
-        ''' Something like the following. It should be non blocking '''
+        ''' function to be called after instanitation to start running the step '''
         self.thread = threading.Thread(target=self.thread_run)
         self.thread.start()
-
-
