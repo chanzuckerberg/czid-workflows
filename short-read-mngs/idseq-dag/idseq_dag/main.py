33644651b393db939702c4d2651beb3dc1694e42
yunfangjuan <yunfang@chanzuckerberg.com>
2018-06-26 11:08:55 -0700

    add version

--- short-read-mngs/idseq-dag/idseq_dag/main.py
+++ short-read-mngs/idseq-dag/idseq_dag/main.py
@@ -7,11 +7,14 @@ import os
 import idseq_dag.util.s3
 import idseq_dag.util.log as log
 from idseq_dag.engine.pipeline_flow import PipelineFlow
+from idseq_dag import __version__
 
 log.configure_logger()
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
     parser.add_argument('dag_json', help='pipeline dag in json file format')
     parser.add_argument('--no-lazy-run', dest='lazy_run', action='store_false')
     parser.add_argument('--key-path-s3', dest='key_path_s3', help='ssh key')
