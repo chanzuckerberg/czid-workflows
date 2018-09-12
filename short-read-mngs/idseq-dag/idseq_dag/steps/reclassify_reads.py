4e2afc2214ecb1cff44091e5e1a0215672a01fd0
yunfangjuan <yunfangjuan@gmail.com>
2018-09-11 17:14:30 -0700

    Reclassify skeleton (#67)
    * update example dag
    
    * reclassify_reads
    
    * skeleton
    
    * fix bug

---
+++ short-read-mngs/idseq-dag/idseq_dag/steps/reclassify_reads.py
@@ -0,0 +1,24 @@
+import json
+from idseq_dag.engine.pipeline_step import PipelineStep
+import idseq_dag.util.command as command
+import idseq_dag.util.count as count
+
+class PipelineStepReclassifyReads(PipelineStep):
+    '''
+        Reclassify reads after alignment
+    '''
+    def run(self):
+        '''
+            Dummy implementation. just copy the files over.
+            Real thing to be implemented later.
+        '''
+        input_files = self.input_files_local[0]
+        output_files = self.output_files_local()
+        for i in range(len(input_files)):
+            command.execute(f"cp {input_files[i]} {output_files[i]}")
+        command.execute(f"echo 1234 > {output_files[4]}")
+
+
+    def count_reads(self):
+        pass
+
