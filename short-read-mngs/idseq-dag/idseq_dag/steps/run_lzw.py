d161a373c178b6de9fcb6a665da508115d7d0c0b
Charles de Bourcy <cdebourcy@chanzuckerberg.com>
2018-10-30 09:53:18 -0700

    require minimum LZW vocabulary length (JIRA-625) (#88)
    * require minimum LZW vocabulary length
    
    * require minimum LZW vocabulary length
    
    * require minimum LZW vocabulary length
    
    * require minimum LZW vocabulary length
    
    * README

--- short-read-mngs/idseq-dag/idseq_dag/steps/run_lzw.py
+++ short-read-mngs/idseq-dag/idseq_dag/steps/run_lzw.py
@@ -28,15 +28,15 @@ class PipelineStepRunLZW(PipelineStep):
     def run(self):
         input_fas = self.input_files_local[0]
         output_fas = self.output_files_local()
-        cutoff_fractions = self.additional_attributes["thresholds"]
-        PipelineStepRunLZW.generate_lzw_filtered(input_fas, output_fas, cutoff_fractions)
+        cutoff_scores = self.additional_attributes["thresholds"]
+        PipelineStepRunLZW.generate_lzw_filtered(input_fas, output_fas, cutoff_scores)
 
     def count_reads(self):
         self.should_count_reads = True
         self.counts_dict[self.name] = count.reads_in_group(self.output_files_local()[0:2])
 
     @staticmethod
-    def lzw_fraction(sequence):
+    def lzw_score(sequence):
         sequence = str(sequence)
         if sequence == "":
             return 0.0
@@ -62,7 +62,16 @@ class PipelineStepRunLZW(PipelineStep):
                 word = c
         if word != "":
             results.append(dictionary[word])
-        return float(len(results)) / len(sequence)
+
+        seq_length = len(sequence)
+        lzw_fraction = float(len(results)) / seq_length
+        if seq_length > 150:
+            # Make sure longer reads don't get excessively penalized
+            adjustment_heuristic = (1 + (seq_length - 150) / 1000) # TODO: revisit 
+            score = lzw_fraction * adjustment_heuristic
+        else:
+            score = lzw_fraction
+        return score
 
     @staticmethod
     def lzw_compute(input_files, slice_step=NUM_SLICES):
@@ -76,13 +85,13 @@ class PipelineStepRunLZW(PipelineStep):
         @run_in_subprocess
         def lzw_compute_slice(slice_start):
             """For each read, or read pair, in input_files, such that read_index % slice_step == slice_start,
-            output the lzw fraction for the read, or the min lzw fraction for the pair."""
-            lzw_fraction = PipelineStepRunLZW.lzw_fraction
+            output the lzw score for the read, or the min lzw score for the pair."""
+            lzw_score = PipelineStepRunLZW.lzw_score
             with open(temp_file_names[slice_start], "a") as slice_output:
                 for i, reads in enumerate(fasta.synchronized_iterator(input_files)):
                     if i % slice_step == slice_start:
-                        lzw_min_fraction = min(lzw_fraction(r.sequence) for r in reads)
-                        slice_output.write(str(lzw_min_fraction) + "\n")
+                        lzw_min_score = min(lzw_score(r.sequence) for r in reads)
+                        slice_output.write(str(lzw_min_score) + "\n")
 
         # slices run in parallel
         mt_map(lzw_compute_slice, range(slice_step))
@@ -96,19 +105,19 @@ class PipelineStepRunLZW(PipelineStep):
         return coalesced_score_file
 
     @staticmethod
-    def generate_lzw_filtered(fasta_files, output_files, cutoff_fractions):
+    def generate_lzw_filtered(fasta_files, output_files, cutoff_scores):
         assert len(fasta_files) == len(output_files)
 
         # This is the bulk of the computation.  Everything else below is just binning by cutoff score.
         coalesced_score_file = PipelineStepRunLZW.lzw_compute(fasta_files)
 
-        cutoff_fractions.sort(reverse=True) # Make sure cutoff is from high to low
+        cutoff_scores.sort(reverse=True) # Make sure cutoff is from high to low
 
         readcount_list = [] # one item per cutoff
         outstreams_list = [] # one item per cutoff
         outfiles_list = [] # one item per cutoff
 
-        for cutoff in cutoff_fractions:
+        for cutoff in cutoff_scores:
             readcount_list.append(0)
             outstreams = []
             outfiles = []
@@ -120,7 +129,7 @@ class PipelineStepRunLZW(PipelineStep):
             outstreams_list.append(outstreams)
             outfiles_list.append(outfiles)
 
-        outstreams_for_cutoff = list(zip(outstreams_list, cutoff_fractions))
+        outstreams_for_cutoff = list(zip(outstreams_list, cutoff_scores))
 
         def score_iterator(score_file: str) -> Iterator[float]:
             with open(score_file, "r") as sf:
@@ -128,10 +137,10 @@ class PipelineStepRunLZW(PipelineStep):
                     yield float(line)
 
         total_reads = 0
-        for reads, fraction in zip(fasta.synchronized_iterator(fasta_files), score_iterator(coalesced_score_file)):
+        for reads, score in zip(fasta.synchronized_iterator(fasta_files), score_iterator(coalesced_score_file)):
             total_reads += 1
             for i, (outstreams, cutoff) in enumerate(outstreams_for_cutoff):
-                if fraction > cutoff:
+                if score > cutoff:
                     readcount_list[i] += 1
                     for ostr, r in zip(outstreams, reads):
                         ostr.write(r.header + "\n")
@@ -148,7 +157,7 @@ class PipelineStepRunLZW(PipelineStep):
         kept_count = 0
         filtered = total_reads
         cutoff_frac = None
-        for cutoff_frac, readcount, outfiles in zip(cutoff_fractions, readcount_list, outfiles_list):
+        for cutoff_frac, readcount, outfiles in zip(cutoff_scores, readcount_list, outfiles_list):
             if readcount > 0:
                 # found the right bin
                 kept_count = readcount
