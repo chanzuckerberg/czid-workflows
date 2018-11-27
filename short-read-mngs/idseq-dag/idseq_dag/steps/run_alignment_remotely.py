13b1fafc034c8148583d689776ed6a04b2722462
Michael Barrientos <mbarrien@users.noreply.github.com>
2018-11-26 16:49:44 -0800

    Change username for gsnap running on Amazon Linux 2 (#98)
    * Change username for gsnap running on Amazon Linux 2
    
    * minor cleanup

--- short-read-mngs/idseq-dag/idseq_dag/steps/run_alignment_remotely.py
+++ short-read-mngs/idseq-dag/idseq_dag/steps/run_alignment_remotely.py
@@ -73,13 +73,11 @@ class PipelineStepRunAlignmentRemotely(PipelineStep):
         sample_name = self.output_dir_s3.rstrip('/').replace('s3://', '').replace('/', '-')
         chunk_size = self.additional_attributes["chunk_size"]
         index_dir_suffix = self.additional_attributes.get("index_dir_suffix")
+        remote_username = "ec2-user"
+        remote_home_dir = "/home/%s" % remote_username
         if service == "gsnap":
-            remote_username = "ubuntu"
-            remote_home_dir = "/home/%s" % remote_username
             remote_index_dir = "%s/share" % remote_home_dir
         elif service == "rapsearch2":
-            remote_username = "ec2-user"
-            remote_home_dir = "/home/%s" % remote_username
             remote_index_dir = "%s/references/nr_rapsearch" % remote_home_dir
 
         if index_dir_suffix:
