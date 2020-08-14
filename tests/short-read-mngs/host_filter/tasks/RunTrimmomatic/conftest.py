dd7d8f0c6592577a9d3affa4665c7708ddb620cf
Andrey Kislyuk <kislyuk@gmail.com>
2020-08-13 21:59:15 -0700

    Rename main workflow to short-read-mngs

---
+++ tests/short-read-mngs/host_filter/tasks/RunTrimmomatic/conftest.py
@@ -0,0 +1,9 @@
+import os
+import pytest
+import WDL
+
+
+@pytest.fixture()
+def exe(repo_dir, load_task):
+    "load the task to be tested"
+    return load_task(os.path.join(repo_dir, "short-read-mngs/host_filter.wdl"), "RunTrimmomatic")
