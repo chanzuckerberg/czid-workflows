8fb8d022c263ab994cbdad3e188513e10c4a8452
morsecodist <todd@morsecodist.io>
2020-12-10 22:30:12 -0500

    optimization

--- short-read-mngs/idseq-dag/idseq_dag/util/idseq_dedup_clusters.py
+++ short-read-mngs/idseq-dag/idseq_dag/util/idseq_dedup_clusters.py
@@ -4,13 +4,13 @@ The first column contains the representative read id of
 a cluster, and the second column contains the read id.
 """
 from csv import DictReader
-from typing import Dict, Optional, Tuple
+from typing import Dict, Optional, List
 import idseq_dag.util.log as log
 from subprocess import run, PIPE
 
 def parse_clusters_file(
     idseq_dedup_clusters_path: str,
-) -> Dict[str, Optional[Tuple]]:
+) -> Dict[str, Optional[List]]:
     clusters_dict = {}
     log.write(f"opening clusters file {idseq_dedup_clusters_path}")
     lines = run(["wc", "-l", idseq_dedup_clusters_path], stdout=PIPE, check=True).stdout.split()[0]
@@ -22,8 +22,9 @@ def parse_clusters_file(
                 log.write(f"parsed row {i}/{lines}")
             r_read_id, read_id = row["representative read id"], row["read id"]
             if r_read_id not in clusters_dict:
-                clusters_dict[r_read_id] = (1,)
+                clusters_dict[r_read_id] = [1]
             else:
                 count, *others = clusters_dict[r_read_id]
-                clusters_dict[r_read_id] = tuple([count + 1] + others + [read_id])
+                clusters_dict[r_read_id][0] += 1
+                clusters_dict[r_read_id].append(read_id)
     return clusters_dict
