052cf81630eb34636fc2fd0063b83c3e285da224
morsecodist <todd@morsecodist.io>
2020-12-10 20:52:56 -0500

    line counts

--- short-read-mngs/idseq-dag/idseq_dag/util/idseq_dedup_clusters.py
+++ short-read-mngs/idseq-dag/idseq_dag/util/idseq_dedup_clusters.py
@@ -6,17 +6,20 @@ a cluster, and the second column contains the read id.
 from csv import DictReader
 from typing import Dict, Optional, Tuple
 import idseq_dag.util.log as log
+from subprocess import run, PIPE
 
 def parse_clusters_file(
     idseq_dedup_clusters_path: str,
 ) -> Dict[str, Optional[Tuple]]:
     clusters_dict = {}
     log.write(f"opening clusters file {idseq_dedup_clusters_path}")
+    lines = run(f"wc -l {idseq_dedup_clusters_path}", stdout=PIPE, check=True).stdout.split()[0]
+    log.write(f"lines in cluster file: {lines}")
     with open(idseq_dedup_clusters_path) as f:
         log.write(f"opened clusters file {idseq_dedup_clusters_path}")
         for i, row in enumerate(DictReader(f)):
-            if i % 100:
-                log.write(f"parsed row {i}")
+            if i % 100 == 0:
+                log.write(f"parsed row {i}/{lines}")
             r_read_id, read_id = row["representative read id"], row["read id"]
             if r_read_id not in clusters_dict:
                 clusters_dict[r_read_id] = (1,)
