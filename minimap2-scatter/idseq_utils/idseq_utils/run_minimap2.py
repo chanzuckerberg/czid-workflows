0efe484b5fbfda38381cd50357d6206772e974f5
Ryan Lim <rlim@chanzuckerberg.com>
2021-09-30 00:41:54 +0000

    add function call for merge

--- minimap2-scatter/idseq_utils/idseq_utils/run_minimap2.py
+++ minimap2-scatter/idseq_utils/idseq_utils/run_minimap2.py
@@ -112,4 +112,5 @@ def run_minimap2(input_dir: str, chunk_dir: str, db_path: str, result_path: str,
     with Pool(MAX_CHUNKS_IN_FLIGHT) as p:
         p.starmap(_run_chunk, chunks)
     run(["s3parcp", "--recursive", chunk_dir, "chunks"], check=True)
-    ("chunks", result_path, *queries)
+    minimap2_merge("chunks", result_path, *queries)
+
