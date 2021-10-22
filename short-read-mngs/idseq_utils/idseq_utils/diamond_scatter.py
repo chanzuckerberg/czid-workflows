1963d55c7653ad2e976f8edc0bfd13aaedd575df
Ryan Lim <rlim@chanzuckerberg.com>
2021-10-22 23:05:03 +0000

    linting

--- short-read-mngs/idseq_utils/idseq_utils/diamond_scatter.py
+++ short-read-mngs/idseq_utils/idseq_utils/diamond_scatter.py
@@ -24,21 +24,38 @@ from Bio.SeqRecord import SeqRecord
 
 def diamond_makedb(cwd: str, reference_fasta: str, database: str):
     cmd = [
-        "diamond", "makedb",
-        "--in", reference_fasta,
-        "--db", database,
+        "diamond",
+        "makedb",
+        "--in",
+        reference_fasta,
+        "--db",
+        database,
     ]
     run(cmd, check=True, cwd=cwd, stdout=PIPE, stderr=PIPE)
 
 
-def diamond_blastx(cwd: str, par_tmpdir: str, block_size: float, database: str, out: str, queries: Iterable[str], chunk = False, join_chunks = 0):
+def diamond_blastx(
+    cwd: str,
+    par_tmpdir: str,
+    block_size: float,
+    database: str,
+    out: str,
+    queries: Iterable[str],
+    chunk=False,
+    join_chunks=0,
+):
     cmd = [
-        "diamond", "blastx",
+        "diamond",
+        "blastx",
         "--multiprocessing",
-        "--parallel-tmpdir", par_tmpdir,
-        "--block-size", str(block_size),
-        "--db", database,
-        "--out", out,
+        "--parallel-tmpdir",
+        par_tmpdir,
+        "--block-size",
+        str(block_size),
+        "--db",
+        database,
+        "--out",
+        out,
     ]
     for query in queries:
         cmd += ["--query", query]
@@ -59,6 +76,7 @@ def diamond_blastx(cwd: str, par_tmpdir: str, block_size: float, database: str, 
 #
 ################################################################################################################
 
+
 def _consume_iter(iterable: Iterable, n: int):
     for i, e in enumerate(iterable):
         yield e
@@ -70,9 +88,9 @@ def align_chunk(ref_chunk: int, start: int, size: int, query_chunk: int):
     return f"{ref_chunk} {start} {size} # query_chunk={query_chunk}\n"
 
 
-def zero_pad(n: int, l: int):
+def zero_pad(n: int, m: int):
     tagged = str(n)
-    return ("0" * (l - len(tagged))) + tagged
+    return ("0" * (m - len(tagged))) + tagged
 
 
 def make_par_dir(cwd: str, par_tmpdir: str):
@@ -118,18 +136,22 @@ def blastx_chunk(db_chunk: str, output_dir: str, *query: str):
         with open(join(tmp_dir, "par-tmp", f"align_todo_{zero_pad(0, 6)}"), "w") as f:
             f.writelines([align_chunk(int(chunk), 0, int(n_seqs), 0)])
         diamond_blastx(
-                cwd=tmp_dir,
-                par_tmpdir="par-tmp",
-                block_size=int(n_letters) / 1e9,
-                database=abspath(db_chunk),
-                out="out.tsv",
-                chunk=True,
-                queries=(abspath(q) for q in query),
+            cwd=tmp_dir,
+            par_tmpdir="par-tmp",
+            block_size=int(n_letters) / 1e9,
+            database=abspath(db_chunk),
+            out="out.tsv",
+            chunk=True,
+            queries=(abspath(q) for q in query),
         )
         ref_block_name = f"ref_block_{zero_pad(0, 6)}_{zero_pad(int(chunk), 6)}"
         ref_dict_name = f"ref_dict_{zero_pad(0, 6)}_{zero_pad(int(chunk), 6)}"
-        shutil.copy(join(tmp_dir, "par-tmp", ref_block_name), join(output_dir, ref_block_name))
-        shutil.copy(join(tmp_dir, "par-tmp", ref_dict_name), join(output_dir, ref_dict_name))
+        shutil.copy(
+            join(tmp_dir, "par-tmp", ref_block_name), join(output_dir, ref_block_name)
+        )
+        shutil.copy(
+            join(tmp_dir, "par-tmp", ref_dict_name), join(output_dir, ref_dict_name)
+        )
 
 
 def mock_reference_fasta(chunks: int, chunk_size: int):
@@ -168,7 +190,7 @@ def blastx_join(chunk_dir: str, out: str, *query: str):
             )
 
         with open(out, "w") as out_f:
-            for out_chunk in glob(join(tmp_dir, f"out.tsv_*")):
+            for out_chunk in glob(join(tmp_dir, "out.tsv_*")):
                 with open(out_chunk) as out_chunk_f:
                     out_f.writelines(out_chunk_f)
                 os.remove(out_chunk)
@@ -186,17 +208,17 @@ if __name__ == "__main__":
     blastx_chunk_parser = subparsers.add_parser("blastx-chunk")
     blastx_chunk_parser.add_argument("--db", required=True)
     blastx_chunk_parser.add_argument("--out-dir", required=True)
-    blastx_chunk_parser.add_argument("--query", required=True, action='append')
+    blastx_chunk_parser.add_argument("--query", required=True, action="append")
 
     blastx_chunks_parser = subparsers.add_parser("blastx-chunks")
     blastx_chunks_parser.add_argument("--db-dir", required=True)
     blastx_chunks_parser.add_argument("--out-dir", required=True)
-    blastx_chunks_parser.add_argument("--query", required=True, action='append')
+    blastx_chunks_parser.add_argument("--query", required=True, action="append")
 
     blastx_join_parser = subparsers.add_parser("blastx-join")
     blastx_join_parser.add_argument("--chunk-dir", required=True)
     blastx_join_parser.add_argument("--out", required=True)
-    blastx_join_parser.add_argument("--query", required=True, action='append')
+    blastx_join_parser.add_argument("--query", required=True, action="append")
 
     args = parser.parse_args(sys.argv[1:])
     if args.command == "make-db":
@@ -204,13 +226,14 @@ if __name__ == "__main__":
     elif args.command == "blastx-chunk":
         blastx_chunk(args.db, args.out_dir, *args.query)
     elif args.command == "blastx-chunks":
+
         def _blastx_chunk(db):
             print(f"STARTING:  {db}")
-            res =  blastx_chunk(join(args.db_dir, db), args.out_dir, *args.query)
+            res = blastx_chunk(join(args.db_dir, db), args.out_dir, *args.query)
             print(f"FINISHING: {db}")
             return res
+
         with Pool(48) as p:
             p.map(_blastx_chunk, os.listdir(args.db_dir))
     elif args.command == "blastx-join":
         blastx_join(args.chunk_dir, args.out, *args.query)
-
