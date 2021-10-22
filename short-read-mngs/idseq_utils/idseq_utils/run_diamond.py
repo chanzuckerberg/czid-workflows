f9bbe7cff782cbf1be745baad0ad9b447a4a26c6
Ryan Lim <rlim@chanzuckerberg.com>
2021-10-22 23:05:03 +0000

    linting

--- short-read-mngs/idseq_utils/idseq_utils/run_diamond.py
+++ short-read-mngs/idseq_utils/idseq_utils/run_diamond.py
@@ -19,11 +19,14 @@ log = logging.getLogger(__name__)
 ALIGNMENT_ALGORITHM = "diamond"
 MAX_CHUNKS_IN_FLIGHT = 10
 
+
 def get_batch_job_desc_bucket():
     try:
         account_id = boto3.client("sts").get_caller_identity()["Account"]
     except ClientError:
-        account_id = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document").json()["accountId"]
+        account_id = requests.get(
+            "http://169.254.169.254/latest/dynamic/instance-identity/document"
+        ).json()["accountId"]
     return f"aegea-batch-jobs-{account_id}"
 
 
@@ -31,15 +34,20 @@ class BatchJobFailed(Exception):
     pass
 
 
-def _log_alignment_batch_job_status(job_id, job_queue, job_definition, chunk_id, status):
-    log.info('alignment_batch_job_status', extra={
-        'job_id': job_id,
-        'chunk_id': chunk_id,
-        'job_queue': job_queue,
-        'job_definition': job_definition,
-        'status': status,
-        'alignment_algorithm': "diamond",
-    })
+def _log_alignment_batch_job_status(
+    job_id, job_queue, job_definition, chunk_id, status
+):
+    log.info(
+        "alignment_batch_job_status",
+        extra={
+            "job_id": job_id,
+            "chunk_id": chunk_id,
+            "job_queue": job_queue,
+            "job_definition": job_definition,
+            "status": status,
+            "alignment_algorithm": "diamond",
+        },
+    )
 
 
 def _get_job_status(job_id):
@@ -49,7 +57,7 @@ def _get_job_status(job_id):
         job_desc_object = batch_job_desc_bucket.Object(key)
         return json.loads(job_desc_object.get()["Body"].read())["status"]
     except ClientError as e:
-        if e.response['Error']['Code'] == 'NoSuchKey':
+        if e.response["Error"]["Code"] == "NoSuchKey":
             # Warn that the object is missing so any issue with the s3 mechanism can be identified
             log.debug("missing_job_description_ojbect", extra={key: key})
             # Return submitted because a missing job status probably means it hasn't been added yet
@@ -67,12 +75,16 @@ def _run_batch_job(job_name, job_queue, job_definition, environment, chunk_id, r
         containerOverrides={
             "environment": environment,
         },
-        retryStrategy={"attempts": retries}
+        retryStrategy={"attempts": retries},
     )
     job_id = response["jobId"]
-    _log_alignment_batch_job_status(job_id, job_queue, job_definition, chunk_id, 'SUBMITTED')
+    _log_alignment_batch_job_status(
+        job_id, job_queue, job_definition, chunk_id, "SUBMITTED"
+    )
 
-    delay = 60 + random.randint(-60 // 2, 60 // 2)  # Add some noise to de-synchronize chunks
+    delay = 60 + random.randint(
+        -60 // 2, 60 // 2
+    )  # Add some noise to de-synchronize chunks
     status = "SUBMITTED"
     # the job this is monitoring has an timeout and the job this runs in has a timeout
     while True:
@@ -80,26 +92,40 @@ def _run_batch_job(job_name, job_queue, job_definition, environment, chunk_id, r
             status = _get_job_status(job_id)
         except ClientError as e:
             # If we get throttled, randomly wait to de-synchronize the requests
-            if e.response['Error']['Code'] == "TooManyRequestsException":
+            if e.response["Error"]["Code"] == "TooManyRequestsException":
                 log.warn("describe_jobs_rate_limit_error", extra={"job_id": job_id})
                 # Possibly implement a backoff here if throttling becomes an issue
             else:
-                log.error("unexpected_client_error_while_polling_job_status", extra={"job_id": job_id})
+                log.error(
+                    "unexpected_client_error_while_polling_job_status",
+                    extra={"job_id": job_id},
+                )
                 raise e
 
         if status == "SUCCEEDED":
-            _log_alignment_batch_job_status(job_id, job_queue, job_definition, chunk_id, status)
+            _log_alignment_batch_job_status(
+                job_id, job_queue, job_definition, chunk_id, status
+            )
             return job_id
         if status == "FAILED":
-            log.error("alignment_batch_job_failed", extra={'job_id': job_id, 'chunk_id': chunk_id, 'alignment_algorithm': ALIGNMENT_ALGORITHM})
-            _log_alignment_batch_job_status(job_id, job_queue, job_definition, chunk_id, status)
+            log.error(
+                "alignment_batch_job_failed",
+                extra={
+                    "job_id": job_id,
+                    "chunk_id": chunk_id,
+                    "alignment_algorithm": ALIGNMENT_ALGORITHM,
+                },
+            )
+            _log_alignment_batch_job_status(
+                job_id, job_queue, job_definition, chunk_id, status
+            )
             raise BatchJobFailed("chunk alignment failed")
         time.sleep(delay)
 
 
 def _db_chunks(bucket: str, prefix):
     s3_client = boto3.client("s3")
-    paginator = s3_client.get_paginator('list_objects_v2')
+    paginator = s3_client.get_paginator("list_objects_v2")
     log.debug("db chunks")
 
     result = paginator.paginate(
@@ -112,35 +138,43 @@ def _db_chunks(bucket: str, prefix):
             yield obj["Key"]
 
 
-def _run_chunk(chunk_id: int, input_dir: str, chunk_dir: str, db_chunk: str, *queries: str):
+def _run_chunk(
+    chunk_id: int, input_dir: str, chunk_dir: str, db_chunk: str, *queries: str
+):
     deployment_environment = os.environ["DEPLOYMENT_ENVIRONMENT"]
     priority_name = os.environ.get("PRIORITY_NAME", "normal")
     alignment_algorithm = "diamond"
     provisioning_model = "EC2"
-    pattern = r's3://.+/samples/([0-9]+)/([0-9]+)/'
+    pattern = r"s3://.+/samples/([0-9]+)/([0-9]+)/"
     m = re.match(pattern, input_dir)
     if m:
         project_id, sample_id = m.group(1), m.group(2)
     else:
-        project_id, sample_id = '0', '0'
+        project_id, sample_id = "0", "0"
 
-    job_name = f"idseq-{deployment_environment}-{alignment_algorithm}-project-{project_id}-sample-{sample_id}-part-{chunk_id}"
+    job_name = (f"idseq-{deployment_environment}-{alignment_algorithm}-"
+                f"project-{project_id}-sample-{sample_id}-part-{chunk_id}")
     job_queue = f"idseq-{deployment_environment}-{alignment_algorithm}-{provisioning_model}-{priority_name}"
     job_definition = f"idseq-{deployment_environment}-{alignment_algorithm}"
 
-    environment = [{
-        "name": "DB_CHUNK",
-        "value": db_chunk,
-    }, {
-        "name": "OUTPUT_DIR",
-        "value": chunk_dir,
-    }]
+    environment = [
+        {
+            "name": "DB_CHUNK",
+            "value": db_chunk,
+        },
+        {
+            "name": "OUTPUT_DIR",
+            "value": chunk_dir,
+        },
+    ]
 
     for i, query in enumerate(queries):
-        environment.append({
-            "name": f"QUERY_{i}",
-            "value": join(input_dir, basename(query)),
-        })
+        environment.append(
+            {
+                "name": f"QUERY_{i}",
+                "value": join(input_dir, basename(query)),
+            }
+        )
 
     _run_batch_job(
         job_name=job_name,
@@ -148,17 +182,21 @@ def _run_chunk(chunk_id: int, input_dir: str, chunk_dir: str, db_chunk: str, *qu
         job_definition=job_definition,
         environment=environment,
         chunk_id=chunk_id,
-        retries=2
+        retries=2,
     )
 
 
-def run_diamond(input_dir: str, chunk_dir: str, db_path: str, result_path: str, *queries: str):
+def run_diamond(
+    input_dir: str, chunk_dir: str, db_path: str, result_path: str, *queries: str
+):
     parsed_url = urlparse(db_path, allow_fragments=False)
     bucket = parsed_url.netloc
-    prefix = parsed_url.path.lstrip('/')
-    chunks = ([chunk_id, input_dir, chunk_dir, f"s3://{bucket}/{db_chunk}", *queries] for chunk_id, db_chunk in enumerate(_db_chunks(bucket, prefix)))
+    prefix = parsed_url.path.lstrip("/")
+    chunks = (
+        [chunk_id, input_dir, chunk_dir, f"s3://{bucket}/{db_chunk}", *queries]
+        for chunk_id, db_chunk in enumerate(_db_chunks(bucket, prefix))
+    )
     with Pool(MAX_CHUNKS_IN_FLIGHT) as p:
         p.starmap(_run_chunk, chunks)
     run(["s3parcp", "--recursive", chunk_dir, "chunks"], check=True)
     blastx_join("chunks", result_path, *queries)
-
