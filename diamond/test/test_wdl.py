c35abc8f654547a146958c9d81dbb79bdf0d25de
morsecodist <todd@morsecodist.io>
2022-02-04 16:03:04 -0500

    dummy tests

---
+++ diamond/test/test_wdl.py
@@ -0,0 +1,6 @@
+from test_util import WDLTestCase
+
+
+class TestDiamond(WDLTestCase):
+    def dummy_test(self):
+        pass
---
+++ minimap2/test/test_wdl.py
@@ -0,0 +1,6 @@
+from test_util import WDLTestCase
+
+
+class TestMinimap2(WDLTestCase):
+    def dummy_test(self):
+        pass
