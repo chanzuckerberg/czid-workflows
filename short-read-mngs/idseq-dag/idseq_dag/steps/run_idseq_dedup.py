173210b2ea578c388642b6d12a413bb589fc0aa6
rzlim08 <37033997+rzlim08@users.noreply.github.com>
2022-02-09 12:47:26 -0800

    add read id to set (#24)
    * add read id to set
    
    * check the right read ids

--- short-read-mngs/idseq-dag/idseq_dag/steps/run_idseq_dedup.py
+++ short-read-mngs/idseq-dag/idseq_dag/steps/run_idseq_dedup.py
@@ -67,8 +67,9 @@ class PipelineStepRunIDSeqDedup(PipelineStep):  # Deliberately not PipelineCount
         #   to guard against potential csv injection
         with open(duplicate_clusters_raw_path) as r, open(duplicate_clusters_path, 'w') as w:
             for row in csv.reader(r):
-                if row[0] in read_ids:
+                if row[1] in read_ids:
                     raise InvalidInputFileError("Input file has duplicate read ids")
+                read_ids.add(row[1])
                 csv.writer(w).writerow(c if c[0].isalnum() else f"'{c}" for c in row)
 
         # Emit cluster sizes.  One line per cluster.  Format "<cluster_size> <cluster_read_id>".
