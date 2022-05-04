9bb354262855f5848fc244dce0f1f6ac3a763652
morsecodist <todd@morsecodist.io>
2022-05-04 15:55:17 -0400

    host genome wdl

---
+++ host-genome-generation/test/test_wdl.py
@@ -0,0 +1,20 @@
+import csv
+import gzip
+import os
+from test_util import WDLTestCase
+
+
+class TestIndexGeneration(WDLTestCase):
+    """Tests the RunValidateInput function"""
+
+    wdl = os.path.join(os.path.dirname(__file__), "..", "host_genome_generation.wdl")
+    common_inputs = {
+        "input_fasta": "fixtures/input.fasta",
+        "host_name": "test",
+        "ercc_fasta": "fixtures/ERCC.fasta",
+        "ercc_gtf": "fixtures/ERCC.gtf",
+    }
+
+    def testIndexGeneration(self):
+        res = self.run_miniwdl()
+        assert res, res
