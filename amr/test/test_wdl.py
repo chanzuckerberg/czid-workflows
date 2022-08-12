2dbd594b62fd635770035d9a9a7468372b12e99b
Ryan Lim <rlim@chanzuckerberg.com>
2022-08-12 23:37:58 +0000

    run black to format test

--- amr/test/test_wdl.py
+++ amr/test/test_wdl.py
@@ -16,19 +16,19 @@ class TestAMR(WDLTestCase):
         with open(res["outputs"]["RunRgiMain.main_amr_results"]) as main_results:
             lastline = main_results.read().splitlines()[-1]
         main = lastline.split("\t")
-        self.assertEqual(main[2], '3')
-        self.assertEqual(main[3], '281')
-    
+        self.assertEqual(main[2], "3")
+        self.assertEqual(main[3], "281")
 
     def testRgiMain(self):
         inputs = {
-            "non_host_reads": [os.path.join(os.path.dirname(__file__), "gsnap_filter_1.fa"),
-                               os.path.join(os.path.dirname(__file__), "gsnap_filter_2.fa")],
+            "non_host_reads": [
+                os.path.join(os.path.dirname(__file__), "gsnap_filter_1.fa"),
+                os.path.join(os.path.dirname(__file__), "gsnap_filter_2.fa"),
+            ],
             "card_json": "s3://czid-public-references/test/AMRv2/card.json",
         }
         res = self.run_miniwdl(task="RunRgiBwtKma", task_input=inputs)
         with open(res["outputs"]["RunRgiBwtKma.kma_amr_results"]) as kma_results:
-            lastline = kma_results.read().splitlines()[-1] 
+            lastline = kma_results.read().splitlines()[-1]
         kma = lastline.split("\t")
-        self.assertEqual(kma[8], 'Elizabethkingia anophelis')
-        
+        self.assertEqual(kma[8], "Elizabethkingia anophelis")
