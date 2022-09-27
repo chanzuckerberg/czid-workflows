4aac0067f2fea9ba5764cb4b0bb658edfb51be61
Ryan Lim <rlim@chanzuckerberg.com>
2022-09-27 23:56:28 +0000

    increment consensus-genome versioning if not found

--- consensus-genome/test/test_wdl.py
+++ consensus-genome/test/test_wdl.py
@@ -11,9 +11,13 @@ from test_util import WDLTestCase
 from Bio import SeqIO
 
 
-nt_s3_path = "s3://czid-public-references/ncbi-indexes-prod/2021-01-22/index-generation-2/nt"
+nt_s3_path = (
+    "s3://czid-public-references/ncbi-indexes-prod/2021-01-22/index-generation-2/nt"
+)
 nt_loc_db = "s3://czid-public-references/ncbi-indexes-prod/2021-01-22/index-generation-2/nt_loc.marisa"
-nr_s3_path = "s3://czid-public-references/ncbi-indexes-prod/2021-01-22/index-generation-2/nr"
+nr_s3_path = (
+    "s3://czid-public-references/ncbi-indexes-prod/2021-01-22/index-generation-2/nr"
+)
 nr_loc_db = "s3://czid-public-references/ncbi-indexes-prod/2021-01-22/index-generation-2/nr_loc.marisa"
 
 
@@ -25,51 +29,73 @@ class TestConsensusGenomes(WDLTestCase):
 
     def test_vadr_error_caught(self):
         # use the long filename error as a proxy for testing VADR error handling
-        consensus = os.path.join(os.path.dirname(__file__), "vadr_input", "really-long-name-consensus.fa")
-        vadr_opts_string = ("-s -r --nomisc --mkey NC_045512 --lowsim5term 2 --lowsim3term 2 --fstlowthr 0.0 "
-                            "--alt_fail lowscore,fsthicnf,fstlocnf")
-        args = ["vadr_model=s3://czid-public-references/consensus-genome/vadr-models-sarscov2-1.2-2.tar.gz",
-                f"vadr_options={vadr_opts_string}",
-                f"assembly={consensus}"]
+        consensus = os.path.join(
+            os.path.dirname(__file__), "vadr_input", "really-long-name-consensus.fa"
+        )
+        vadr_opts_string = (
+            "-s -r --nomisc --mkey NC_045512 --lowsim5term 2 --lowsim3term 2 --fstlowthr 0.0 "
+            "--alt_fail lowscore,fsthicnf,fstlocnf"
+        )
+        args = [
+            "vadr_model=s3://czid-public-references/consensus-genome/vadr-models-sarscov2-1.2-2.tar.gz",
+            f"vadr_options={vadr_opts_string}",
+            f"assembly={consensus}",
+        ]
         res = self.run_miniwdl(args=args, task="Vadr", task_input={"prefix": ""})
         self.assertIsNotNone(res["outputs"]["Vadr.vadr_errors"])
         self.assertEqual(res["outputs"]["Vadr.vadr_alerts"], None)
         self.assertEqual(res["outputs"]["Vadr.vadr_quality"], None)
 
     def test_vadr_flag_works(self):
-        consensus = os.path.join(os.path.dirname(__file__), "vadr_input", "really-long-name-consensus.fa")
-        vadr_opts_string = ("-s -r --nomisc --mkey sarscov2 --lowsim5term 2 --lowsim3term 2 "
-                            "--fstlowthr 0.0 --alt_fail lowscore,fsthicnf,fstlocnf --noseqnamemax")
-        args = ["vadr_model=s3://czid-public-references/consensus-genome/vadr-models-sarscov2-1.2-2.tar.gz",
-                f"vadr_options={vadr_opts_string}",
-                f"assembly={consensus}"]
+        consensus = os.path.join(
+            os.path.dirname(__file__), "vadr_input", "really-long-name-consensus.fa"
+        )
+        vadr_opts_string = (
+            "-s -r --nomisc --mkey sarscov2 --lowsim5term 2 --lowsim3term 2 "
+            "--fstlowthr 0.0 --alt_fail lowscore,fsthicnf,fstlocnf --noseqnamemax"
+        )
+        args = [
+            "vadr_model=s3://czid-public-references/consensus-genome/vadr-models-sarscov2-1.2-2.tar.gz",
+            f"vadr_options={vadr_opts_string}",
+            f"assembly={consensus}",
+        ]
         res = self.run_miniwdl(args=args, task="Vadr", task_input={"prefix": ""})
         self.assertIsNotNone(res["outputs"]["Vadr.vadr_alerts"])
         self.assertIsNone(res["outputs"]["Vadr.vadr_errors"])
 
     # test the depths associated with SNAP ivar trim -x 5
     def test_sars_cov2_illumina_cg_snap(self):
-        aligned_reads = os.path.join(os.path.dirname(__file__), "trim_primers_input", "snap_aligned_reads.bam")
-        args = [f"alignments={aligned_reads}",
-                "primer_bed=s3://czid-public-references/consensus-genome/snap_primers.bed"]
+        aligned_reads = os.path.join(
+            os.path.dirname(__file__), "trim_primers_input", "snap_aligned_reads.bam"
+        )
+        args = [
+            f"alignments={aligned_reads}",
+            "primer_bed=s3://czid-public-references/consensus-genome/snap_primers.bed",
+        ]
         res = self.run_miniwdl(args, task="TrimPrimers", task_input={"prefix": ""})
-        with open(res["outputs"]["TrimPrimers.trimmed_bam_bai"], 'rb') as f:
+        with open(res["outputs"]["TrimPrimers.trimmed_bam_bai"], "rb") as f:
             hash = hashlib.md5(f.read()).hexdigest()
         self.assertEqual(hash, "3081c5e09cc31194821a84fc24b5685f")
-        with open(res["outputs"]["TrimPrimers.trimmed_bam_ch"], 'rb') as f:
+        with open(res["outputs"]["TrimPrimers.trimmed_bam_ch"], "rb") as f:
             hash = hashlib.md5(f.read()).hexdigest()
         self.assertEqual(hash, "a4d8b1d6e5d6a0bbc4b336919baddffd")
 
     # test the depths associated with tailedseq protocol, ivar trim -x 2
     def test_sars_cov2_illumina_cg_tailedseq(self):
-        aligned_reads = os.path.join(os.path.dirname(__file__), "trim_primers_input", "tailedseq_aligned_reads.bam")
-        args = [f"alignments={aligned_reads}",
-                "primer_bed=s3://czid-public-references/consensus-genome/artic_v3_short_275_primers.bed"]
+        aligned_reads = os.path.join(
+            os.path.dirname(__file__),
+            "trim_primers_input",
+            "tailedseq_aligned_reads.bam",
+        )
+        args = [
+            f"alignments={aligned_reads}",
+            "primer_bed=s3://czid-public-references/consensus-genome/artic_v3_short_275_primers.bed",
+        ]
         res = self.run_miniwdl(args, task="TrimPrimers", task_input={"prefix": ""})
-        with open(res["outputs"]["TrimPrimers.trimmed_bam_bai"], 'rb') as f:
+        with open(res["outputs"]["TrimPrimers.trimmed_bam_bai"], "rb") as f:
             hash = hashlib.md5(f.read()).hexdigest()
         self.assertEqual(hash, "010dfb9df3c7a45c9bf6556925859ce7")
-        with open(res["outputs"]["TrimPrimers.trimmed_bam_ch"], 'rb') as f:
+        with open(res["outputs"]["TrimPrimers.trimmed_bam_ch"], "rb") as f:
             hash = hashlib.md5(f.read()).hexdigest()
         self.assertEqual(hash, "7028bf8450548391f264f2948b9e19f0")
 
@@ -80,10 +106,15 @@ class TestConsensusGenomes(WDLTestCase):
         models = ["r10_min_high_g340", "r941_prom_fast_g303"]
         fastq = os.path.join(os.path.dirname(__file__), "no_host_1.fq.gz")
         for model in models:
-            args = ["prefix=''", "sample=test_sample", f"fastqs={fastq}",
-                    "normalise=1000", f"medaka_model={model}",
-                    "primer_schemes=s3://czid-public-references/consensus-genome/artic-primer-schemes_v2.tar.gz",
-                    "primer_set=nCoV-2019/V1200"]
+            args = [
+                "prefix=''",
+                "sample=test_sample",
+                f"fastqs={fastq}",
+                "normalise=1000",
+                f"medaka_model={model}",
+                "primer_schemes=s3://czid-public-references/consensus-genome/artic-primer-schemes_v2.tar.gz",
+                "primer_set=nCoV-2019/V1200",
+            ]
             res = self.run_miniwdl(args, task="RunMinion")
             for filename in res["outputs"].values():
                 self.assertGreater(os.path.getsize(filename), 0)
@@ -99,10 +130,15 @@ class TestConsensusGenomes(WDLTestCase):
         """
         model = "r941_prom_snp_g360"
         fastq = os.path.join(os.path.dirname(__file__), "no_host_1.fq.gz")
-        args = ["prefix=''", "sample=test_sample", f"fastqs={fastq}",
-                "normalise=1000", f"medaka_model={model}",
-                "primer_schemes=s3://czid-public-references/consensus-genome/artic-primer-schemes.tar.gz",
-                "primer_set=nCoV-2019/V3"]
+        args = [
+            "prefix=''",
+            "sample=test_sample",
+            f"fastqs={fastq}",
+            "normalise=1000",
+            f"medaka_model={model}",
+            "primer_schemes=s3://czid-public-references/consensus-genome/artic-primer-schemes.tar.gz",
+            "primer_set=nCoV-2019/V3",
+        ]
         with self.assertRaises(CalledProcessError) as ecm:
             self.run_miniwdl(args, task="RunMinion")
         miniwdl_error = json.loads(ecm.exception.output)
@@ -113,8 +149,14 @@ class TestConsensusGenomes(WDLTestCase):
         """
         Test that Quast will run with midnight primers
         """
-        assembly = os.path.join(os.path.dirname(__file__), "quast_input", "test_sample.consensus.fasta")
-        bam = os.path.join(os.path.dirname(__file__), "quast_input", "test_sample.primertrimmed.rg.sorted.bam")
+        assembly = os.path.join(
+            os.path.dirname(__file__), "quast_input", "test_sample.consensus.fasta"
+        )
+        bam = os.path.join(
+            os.path.dirname(__file__),
+            "quast_input",
+            "test_sample.primertrimmed.rg.sorted.bam",
+        )
         fastq = os.path.join(os.path.dirname(__file__), "no_host_1.fq.gz")
         args = [
             "prefix=''",
@@ -124,7 +166,7 @@ class TestConsensusGenomes(WDLTestCase):
             "no_reads_quast=false",
             "technology=ONT",
             "primer_schemes=s3://czid-public-references/consensus-genome/artic-primer-schemes_v2.tar.gz",
-            "primer_set=nCoV-2019/V1200"
+            "primer_set=nCoV-2019/V1200",
         ]
         res = self.run_miniwdl(args, task="Quast")
         for filename in res["outputs"]["Quast.quast_dir"]:
@@ -147,43 +189,79 @@ class TestConsensusGenomes(WDLTestCase):
         )
         for output_name, output in res["outputs"].items():
             for filename in output:
-                if output_name == 'ValidateInput.input_stats' or output_name == 'ValidateInput.seqkit_version':
+                if (
+                    output_name == "ValidateInput.input_stats"
+                    or output_name == "ValidateInput.seqkit_version"
+                ):
                     continue
                 self.assertTrue(filename.endswith(".fastq.gz"))
                 self.assertGreater(os.path.getsize(filename), 0)
 
     def test_zip_outputs(self):
-        res = self.run_miniwdl(task="ZipOutputs", args=[
-            "prefix=test",
-            f"outputFiles={self.wdl}",
-        ])
+        res = self.run_miniwdl(
+            task="ZipOutputs", args=["prefix=test", f"outputFiles={self.wdl}"]
+        )
         with zipfile.ZipFile(res["outputs"]["ZipOutputs.output_zip"]) as fh:
             self.assertEqual(fh.namelist(), ["run.wdl"])
 
     def test_fetch_sequence_by_accession_id(self):
-        res = self.run_miniwdl(task="FetchSequenceByAccessionId", args=[
-            "accession_id=HIN71308.1",
-            f"nt_s3_path={nt_s3_path}",
-            f"nt_loc_db={nt_loc_db}",
-            f"nr_s3_path={nr_s3_path}",
-            f"nr_loc_db={nr_loc_db}",
-        ])
-        with open(res["outputs"]["FetchSequenceByAccessionId.sequence_fa"]) as fh:
-            self.assertEqual(fh.readline().strip(), ">HIN71308.1 hypothetical protein [Dehalococcoidia bacterium]")
-            self.assertTrue(fh.readline().startswith("MSMIIQSGKLTQTRVEAQHMDFEPRYTEEQQTFRTEVRDWLKDNVPDDLANPADSAD"))
-
-        with self.assertRaises(CalledProcessError) as ecm:
-            self.run_miniwdl(task="FetchSequenceByAccessionId", args=[
-                "accession_id=NO_ACCESSION_ID",
+        res = self.run_miniwdl(
+            task="FetchSequenceByAccessionId",
+            args=[
+                "accession_id=HIN71308.1",
                 f"nt_s3_path={nt_s3_path}",
                 f"nt_loc_db={nt_loc_db}",
                 f"nr_s3_path={nr_s3_path}",
                 f"nr_loc_db={nr_loc_db}",
-            ])
-        self.assertRunFailed(ecm, task="FetchSequenceByAccessionId",
-                             error="AccessionIdNotFound",
-                             cause=("The Accession ID was not found in the CZ ID database, "
-                                    "so a generalized consensus genome could not be run"))
+            ],
+        )
+        with open(res["outputs"]["FetchSequenceByAccessionId.sequence_fa"]) as fh:
+            self.assertEqual(
+                fh.readline().strip(),
+                ">HIN71308.1 hypothetical protein [Dehalococcoidia bacterium]",
+            )
+            self.assertTrue(
+                fh.readline().startswith(
+                    "MSMIIQSGKLTQTRVEAQHMDFEPRYTEEQQTFRTEVRDWLKDNVPDDLANPADSAD"
+                )
+            )
+
+        res = self.run_miniwdl(
+            task="FetchSequenceByAccessionId",
+            args=[
+                "accession_id=MN996532.1",
+                f"nt_s3_path={nt_s3_path}",
+                f"nt_loc_db={nt_loc_db}",
+                f"nr_s3_path={nr_s3_path}",
+                f"nr_loc_db={nr_loc_db}",
+            ],
+        )
+        with open(res["outputs"]["FetchSequenceByAccessionId.sequence_fa"]) as fh:
+            self.assertEqual(
+                fh.readline().strip(),
+                ">MN996532.2 Bat coronavirus RaTG13, complete genome",
+            )
+
+        with self.assertRaises(CalledProcessError) as ecm:
+            self.run_miniwdl(
+                task="FetchSequenceByAccessionId",
+                args=[
+                    "accession_id=NO_ACCESSION_ID",
+                    f"nt_s3_path={nt_s3_path}",
+                    f"nt_loc_db={nt_loc_db}",
+                    f"nr_s3_path={nr_s3_path}",
+                    f"nr_loc_db={nr_loc_db}",
+                ],
+            )
+        self.assertRunFailed(
+            ecm,
+            task="FetchSequenceByAccessionId",
+            error="AccessionIdNotFound",
+            cause=(
+                "The Accession ID 'NO_ACCESSION_ID' was not found in the CZ ID database, "
+                "so a generalized consensus genome could not be run"
+            ),
+        )
 
     def test_no_coverage_error(self):
         ref_host_blank = os.path.join(os.path.dirname(__file__), "blank.fastq.gz")
@@ -192,24 +270,32 @@ class TestConsensusGenomes(WDLTestCase):
         fastqs_blank = os.path.join(os.path.dirname(__file__), "blank.fastq.gz")
         cleaned_bam = os.path.join(os.path.dirname(__file__), "empty.bam")
         with self.assertRaises(CalledProcessError) as ecm:
-            self.run_miniwdl(task="ComputeStats", args=[
-                f"ref_host={ref_host_blank}",
-                f"assembly={assembly_blank}",
-                f"vcf={vcf_blank}",
-                f"fastqs={fastqs_blank}",
-                "technology=Illumina",
-                f"cleaned_bam={cleaned_bam}"],
-                task_input={
-                    "sample": "sample",
-                    "prefix": "",
-                })
-        self.assertRunFailed(ecm, task="ComputeStats",
-                             error="InsufficientReadsError",
-                             cause="There was insufficient coverage so a consensus genome could not be created.")
+            self.run_miniwdl(
+                task="ComputeStats",
+                args=[
+                    f"ref_host={ref_host_blank}",
+                    f"assembly={assembly_blank}",
+                    f"vcf={vcf_blank}",
+                    f"fastqs={fastqs_blank}",
+                    "technology=Illumina",
+                    f"cleaned_bam={cleaned_bam}",
+                ],
+                task_input={"sample": "sample", "prefix": ""},
+            )
+        self.assertRunFailed(
+            ecm,
+            task="ComputeStats",
+            error="InsufficientReadsError",
+            cause="There was insufficient coverage so a consensus genome could not be created.",
+        )
 
     def test_max_reads_illumina(self):
-        fastq_0 = os.path.join(os.path.dirname(__file__), "SRR11741455_65054_nh_R1.fastq.gz")
-        fastq_1 = os.path.join(os.path.dirname(__file__), "SRR11741455_65054_nh_R2.fastq.gz")
+        fastq_0 = os.path.join(
+            os.path.dirname(__file__), "SRR11741455_65054_nh_R1.fastq.gz"
+        )
+        fastq_1 = os.path.join(
+            os.path.dirname(__file__), "SRR11741455_65054_nh_R2.fastq.gz"
+        )
 
         res = self.run_miniwdl(
             task="ValidateInput",
@@ -221,11 +307,13 @@ class TestConsensusGenomes(WDLTestCase):
             },
         )
         for output in res["outputs"]["ValidateInput.validated_fastqs"]:
-            with gzip.open(output, 'rt') as f:
+            with gzip.open(output, "rt") as f:
                 self.assertEqual(sum(1 for _ in SeqIO.parse(f, "fastq")), 100)
 
     def test_max_reads_ont(self):
-        fastq = os.path.join(os.path.dirname(__file__), "SRR11741455_65054_nh_R1.fastq.gz")
+        fastq = os.path.join(
+            os.path.dirname(__file__), "SRR11741455_65054_nh_R1.fastq.gz"
+        )
 
         res = self.run_miniwdl(
             task="ValidateInput",
@@ -237,12 +325,14 @@ class TestConsensusGenomes(WDLTestCase):
             },
         )
         for output in res["outputs"]["ValidateInput.validated_fastqs"]:
-            with gzip.open(output, 'rt') as f:
+            with gzip.open(output, "rt") as f:
                 self.assertEqual(sum(1 for _ in SeqIO.parse(f, "fastq")), 100)
 
     def test_max_reads_uncompressed_input(self):
-        fastq = os.path.join(os.path.dirname(__file__), "SRR11741455_65054_nh_R1.fastq.gz")
-        with tempfile.NamedTemporaryFile('wb') as f, gzip.open(fastq) as gzipped_f:
+        fastq = os.path.join(
+            os.path.dirname(__file__), "SRR11741455_65054_nh_R1.fastq.gz"
+        )
+        with tempfile.NamedTemporaryFile("wb") as f, gzip.open(fastq) as gzipped_f:
             f.write(gzipped_f.read())
 
             res = self.run_miniwdl(
@@ -252,8 +342,8 @@ class TestConsensusGenomes(WDLTestCase):
                     "technology": "ONT",
                     "fastqs": [f.name],
                     "prefix": "",
-                }
+                },
             )
         for output in res["outputs"]["ValidateInput.validated_fastqs"]:
-            with gzip.open(output, 'rt') as f:
+            with gzip.open(output, "rt") as f:
                 self.assertEqual(sum(1 for _ in SeqIO.parse(f, "fastq")), 100)
