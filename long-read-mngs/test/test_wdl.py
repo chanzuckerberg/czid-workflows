e0fb72e5fbbaca700ca6060b6189da26ba6e288e
morsecodist <todd@morsecodist.io>
2022-10-05 13:11:28 -0700

    working tests

--- long-read-mngs/test/test_wdl.py
+++ long-read-mngs/test/test_wdl.py
@@ -10,10 +10,11 @@ class TestLongReadMNGS(WDLTestCase):
     wdl = os.path.join(os.path.dirname(__file__), "..", "run.wdl")
     common_inputs = {
         "s3_wd_uri": "",
-        "input_fastq": os.path.join(os.path.dirname(__file__), "test_files/dummy.fastq"),
-        "minimap_host_db": os.path.join(os.path.dirname(__file__), "test_files/human.fasta"),
-        "minimap_human_db": os.path.join(os.path.dirname(__file__), "test_files/human.fasta"),
-        "minimap2_local_db_path": os.path.join(os.path.dirname(__file__), "test_files/viral_nt_minimap2_index"),
+        "input_fastq": os.path.join(os.path.dirname(__file__), "test_files/test.fastq"),
+        "minimap_host_db": "s3://czid-public-references/host_filter/ercc/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/ERCC.fasta",
+        "minimap_human_db": "s3://czid-public-references/host_filter/ercc/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/ERCC.fasta",
+        "minimap2_local_db_path": "s3://czid-public-references/test/viral-alignment-indexes/viral_nt",
+        "diamond_local_db_path": "s3://czid-public-references/test/viral-alignment-indexes/viral_nr",
         "accession2taxid_db": "s3://czid-public-references/mini-database/alignment_indexes/2020-08-20-viral/viral_accessions2taxid.marisa",
         "lineage_db": "s3://czid-public-references/taxonomy/2021-01-22/taxid-lineages.db",
         "taxon_blacklist": "s3://czid-public-references/taxonomy/2021-01-22/taxon_blacklist.txt",
@@ -23,5 +24,4 @@ class TestLongReadMNGS(WDLTestCase):
     def testLongReadMNGS(self):
         res = self.run_miniwdl([])
         outputs = res["outputs"]
-        with gzip.open(outputs["czid_long_read_mngs.foo"], "rt") as f:
-            pass
+        self.assertIn("czid_long_read_mngs.nt_deduped_out_m8", outputs)
