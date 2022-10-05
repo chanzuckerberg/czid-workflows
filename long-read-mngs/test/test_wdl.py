60c23bb5cd03b6b475e18d5f543e667bf29c6e16
morsecodist <todd@morsecodist.io>
2022-10-05 13:45:17 -0700

    lint

--- long-read-mngs/test/test_wdl.py
+++ long-read-mngs/test/test_wdl.py
@@ -1,5 +1,3 @@
-import csv
-import gzip
 import os
 from test_util import WDLTestCase
 
@@ -8,17 +6,20 @@ class TestLongReadMNGS(WDLTestCase):
     """Tests the czid_long_read_mngs workflow"""
 
     wdl = os.path.join(os.path.dirname(__file__), "..", "run.wdl")
+    ref_bucket = "s3://czid-public-references"
+    ercc_prefix = "host_filter/ercc/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime"
+    alignment_indexes_prefix = "mini-database/alignment_indexes/2020-08-20-viral"
     common_inputs = {
         "s3_wd_uri": "",
         "input_fastq": os.path.join(os.path.dirname(__file__), "test_files/test.fastq"),
-        "minimap_host_db": "s3://czid-public-references/host_filter/ercc/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/ERCC.fasta",
-        "minimap_human_db": "s3://czid-public-references/host_filter/ercc/2018-02-15-utc-1518652800-unixtime__2018-02-15-utc-1518652800-unixtime/ERCC.fasta",
-        "minimap2_local_db_path": "s3://czid-public-references/test/viral-alignment-indexes/viral_nt",
-        "diamond_local_db_path": "s3://czid-public-references/test/viral-alignment-indexes/viral_nr",
-        "accession2taxid_db": "s3://czid-public-references/mini-database/alignment_indexes/2020-08-20-viral/viral_accessions2taxid.marisa",
-        "lineage_db": "s3://czid-public-references/taxonomy/2021-01-22/taxid-lineages.db",
-        "taxon_blacklist": "s3://czid-public-references/taxonomy/2021-01-22/taxon_blacklist.txt",
-        "deuterostome_db": "s3://czid-public-references/taxonomy/2021-01-22/deuterostome_taxids.txt",
+        "minimap_host_db": os.path.join(ref_bucket, ercc_prefix, "ERCC.fasta"),
+        "minimap_human_db": os.path.join(ref_bucket, ercc_prefix, "ERCC.fasta"),
+        "minimap2_local_db_path": os.path.join(ref_bucket, "test/viral-alignment-indexes/viral_nt"),
+        "diamond_local_db_path": os.path.join(ref_bucket, "test/viral-alignment-indexes/viral_nr"),
+        "accession2taxid_db": os.path.join(ref_bucket, alignment_indexes_prefix, "viral_accessions2taxid.marisa"),
+        "lineage_db": os.path.join(ref_bucket, "taxonomy/2021-01-22/taxid-lineages.db"),
+        "taxon_blacklist": os.path.join(ref_bucket, "taxonomy/2021-01-22/taxon_blacklist.txt"),
+        "deuterostome_db": os.path.join(ref_bucket, "taxonomy/2021-01-22/deuterostome_taxids.txt"),
     }
 
     def testLongReadMNGS(self):
