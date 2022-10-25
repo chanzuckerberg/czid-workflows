d541adf514705c1354269470e77ece8501edb217
morsecodist <todd@morsecodist.io>
2022-10-25 11:39:07 -0700

    lint

--- short-read-mngs/s3quilt/__init__.py
+++ short-read-mngs/s3quilt/__init__.py
@@ -2,5 +2,6 @@ from typing import Iterable
 from .go import Slice_uint64
 from . import s3quilt
 
+
 def download_chunks(bucket: str, key: str, output_file_path: str, starts: Iterable[int], lengths: Iterable[int]):
     s3quilt.DownloadChunks(bucket, key, output_file_path, Slice_uint64(starts), Slice_uint64(lengths))
