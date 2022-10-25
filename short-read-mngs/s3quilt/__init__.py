09a94533e183174f3ac059f0fd052c20da152558
morsecodist <todd@morsecodist.io>
2022-10-25 12:52:46 -0700

    relative paths maybe

--- short-read-mngs/s3quilt/__init__.py
+++ short-read-mngs/s3quilt/__init__.py
@@ -1,7 +1,12 @@
+import os
+import sys
 from typing import Iterable
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from .go import Slice_uint64
-from . import s3quilt
+from .s3quilt import DownloadChunks
 
 
 def download_chunks(bucket: str, key: str, output_file_path: str, starts: Iterable[int], lengths: Iterable[int]):
-    s3quilt.DownloadChunks(bucket, key, output_file_path, Slice_uint64(starts), Slice_uint64(lengths))
+    DownloadChunks(bucket, key, output_file_path, Slice_uint64(starts), Slice_uint64(lengths))
