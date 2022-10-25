b1ec26923646aef5e74afab762f6df747d88d461
morsecodist <todd@morsecodist.io>
2022-10-25 13:38:08 -0700

    add init

--- short-read-mngs/s3quilt/__init__.py
+++ short-read-mngs/s3quilt/__init__.py
@@ -5,8 +5,6 @@ from typing import Iterable
 from .go import Slice_uint64
 from .s3quilt import DownloadChunks
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
 
 def download_chunks(bucket: str, key: str, output_file_path: str, starts: Iterable[int], lengths: Iterable[int]):
     DownloadChunks(bucket, key, output_file_path, Slice_uint64(starts), Slice_uint64(lengths))
