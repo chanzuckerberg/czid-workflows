8aa01e959c1df78b931e9b1ed1cd6e7811dafbc1
morsecodist <todd@morsecodist.io>
2022-11-01 14:09:28 -0700

    more workflows

--- workflows/short-read-mngs/test/conftest.py
+++ workflows/short-read-mngs/test/conftest.py
@@ -12,10 +12,10 @@ def short_read_mngs_bench3_viral_outputs(util):
         util.repo_dir() / "workflows/short-read-mngs/local_driver.wdl",
         "docker_image_id=" + os.environ.get("DOCKER_IMAGE_ID", "idseq-short-read-mngs"),
         "fastqs_0=",
-        util.repo_dir()
+        util.repo_dir() / "workflows"
         / "short-read-mngs/test/norg_6__nacc_27__uniform_weight_per_organism__hiseq_reads__v6__R1.fastq.gz",
         "fastqs_1=",
-        util.repo_dir()
+        util.repo_dir() / "workflows"
         / "short-read-mngs/test/norg_6__nacc_27__uniform_weight_per_organism__hiseq_reads__v6__R2.fastq.gz",
         "-i",
         util.repo_dir() / "workflows/short-read-mngs/test/local_test_viral.yml",
