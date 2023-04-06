4f4a6c06ca09ae375f2216414b61a7634883bdfd
Robert Aboukhalil <raboukhalil@chanzuckerberg.com>
2023-04-06 15:25:37 -0700

    Draft of script to convert comprehensive_AMR_metrics.tsv to BAM

---
+++ workflows/amr/tsv_to_sam.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# TODO: Figure out where contigs.fasta is depending on whether ran mNGS?
+PATH_CONTIGS="contigs.fasta"
+PATH_COMPREHENSIVE_REPORT="final_reports/comprehensive_AMR_metrics.tsv"
+PATH_OUTPUT_SAM="test.sam"
+PATH_OUTPUT_BAM="test.bam"
+
+# Create index to enable querying fasta file
+samtools faidx $PATH_CONTIGS
+
+# Create SAM header with mock sequence sizes
+echo -e "@HD\tVN:1.0\tSO:unsorted" > $PATH_OUTPUT_SAM
+awk -F "\t" -v OFS="\t" '{
+  contigName = $2
+  geneId = $36
+
+  # Ignore header, and lines with no info
+  if(NR == 1 || contigName == "" || geneId == "")
+    next;
+
+  print "@SQ", "SN:"geneId, "LN:100"
+}' $PATH_COMPREHENSIVE_REPORT | sort | uniq >> $PATH_OUTPUT_SAM
+
+# Go through each line of the TSV and output a SAM record
+awk -F "\t" -v OFS="\t" -v PATH_CONTIGS="$PATH_CONTIGS" '{
+  contigName = $2
+  geneId = $36
+
+  # Ignore header, and lines with no info
+  if(NR == 1 || contigName == "" || geneId == "")
+    next;
+
+  # Contig names here have an additional "_" followed by a number at the end,
+  # but the contig name in contigs.fasta does not, so remove it.
+  gsub(/_[^_]*$/, "", contigName);
+
+  # Fetch contig sequence
+  "samtools faidx -n 0 " PATH_CONTIGS " \"" contigName "\" | tail -n 1" | getline contigSequence
+
+  print contigName, 0, geneId, "1", 255, "*", "*", 0, 0, contigSequence, "*"
+}' $PATH_COMPREHENSIVE_REPORT >> $PATH_OUTPUT_SAM
+
+# Convert SAM to BAM and index (ignore warning about no CIGAR string; we don't need those)
+samtools sort -o $PATH_OUTPUT_BAM $PATH_OUTPUT_SAM 2>/dev/null
+samtools index $PATH_OUTPUT_BAM
+
+# Test: samtools view -h test.bam "ARO:3000777|ID:153|Name:adeF|NCBI:CT025801.2" | samtools fasta
