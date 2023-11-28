1771c117478c0ad2e77353ba08f62bf945937ad0
phoenixAja <plogan@chanzuckerberg>
2023-11-28 10:57:00 -0800

    do groupings of sequence length in parallel

--- workflows/index-generation/ncbi-compress/src/fasta_tools.rs
+++ workflows/index-generation/ncbi-compress/src/fasta_tools.rs
@@ -1,11 +1,11 @@
 pub mod fasta_tools {
-    use std::fs;
     use std::path::Path;
     use std::fs::OpenOptions;
+    use std::{borrow::BorrowMut, fs};
 
     use bio::io::fasta;
-
-    use crate::logging::logging;
+    use rayon::prelude::*;
+    // use rayon::iter::IntoParallelRefIterator;
 
     pub fn get_filename(sequence_length: usize) -> String {
         let min_length = (sequence_length/1000)*1000;
@@ -17,37 +17,58 @@ pub mod fasta_tools {
         return format!("sequences_{}-{}.fa", min_length_padded, max_length_padded)
     }
 
-    pub fn break_up_fasta_by_sequence_length<P: AsRef<Path> + std::fmt::Debug + std::fmt::Display>(
+    fn process_seq_chunk<P: std::fmt::Display>(record: &fasta::Record, output_directory: &P) -> () {
+        //let record = c.unwrap();
+        let sequence_length = record.seq().len();
+        let filename = get_filename(sequence_length);
+        let output_path = format!("{}/{}", output_directory, filename);
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open(output_path)
+            .expect("Error opening fasta file");
+        let mut writer = fasta::Writer::new(file);
+        writer.write_record(&record).expect("Error writing record");
+        // current_count += 1;
+
+    }
+
+
+    pub fn break_up_fasta_by_sequence_length<P: AsRef<Path> + std::fmt::Debug + std::fmt::Display + std::marker::Send + std::marker::Sync>(
         input_fasta_path: P,
         output_directory: P,
         total_sequence_count: usize,
      ) {
         let mut current_count = 0;
         fs::create_dir_all(&output_directory).expect("Error creating output directory");
-        let reader = fasta::Reader::from_file(input_fasta_path).expect("Error reading fasta file"); 
-        reader.records().enumerate().for_each(|(_i, record)| {
-            let record = record.expect("Error reading record");
-            let sequence_length = record.seq().len();
-            let filename = get_filename(sequence_length);
-            let output_path = format!("{}/{}", output_directory, filename);
-            let file = OpenOptions::new()
-                .write(true)
-                .append(true)
-                .create(true)
-                .open(output_path)
-                .expect("Error opening fasta file");
-            let mut writer = fasta::Writer::new(file);
-            writer.write_record(&record).expect("Error writing record");
-            current_count += 1;
+        let reader = fasta::Reader::from_file(&input_fasta_path).unwrap(); 
+        let mut records_iter = reader.records();
+        let chunk_size = 5;
+
+        // create initial chunk of records
+        let mut chunk = records_iter
+        .borrow_mut()
+        .take(chunk_size)
+        .collect::<Vec<_>>();
+
+        while chunk.len() > 0 {
+            chunk.par_iter().filter_map(|r| {
+                let rec = r.as_ref().expect("Error reading record");
+                process_seq_chunk(rec, &output_directory);
+                Some(())
+            }).collect::<Vec<_>>();
+            current_count += chunk.len();
             if current_count % 100000 == 0 {
                 let processed_percentage = (current_count as f64 / total_sequence_count as f64) * 100.0;
                 log::info!("{} of sequences processed", processed_percentage);
             }
-        });
-
+            // refill chunk with new records from iterator
+            chunk = records_iter
+            .borrow_mut()
+            .take(chunk_size)
+            .collect::<Vec<_>>();
+        }
+        log::info!("all sequences processed");
     }
-
-
-
-
 }
