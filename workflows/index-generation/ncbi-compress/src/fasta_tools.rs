177a07b61ba25e809de729c3127d50df2ec1c7e2
phoenixAja <plogan@chanzuckerberg>
2023-11-28 13:22:55 -0800

    renaming

--- workflows/index-generation/ncbi-compress/src/fasta_tools.rs
+++ workflows/index-generation/ncbi-compress/src/fasta_tools.rs
@@ -2,14 +2,12 @@ pub mod fasta_tools {
     use std::path::Path;
     use std::fs::OpenOptions;
     use std::{borrow::BorrowMut, fs};
+    use std::collections::HashMap;
+    use std::sync::Mutex;
+
 
     use bio::io::fasta;
     use rayon::prelude::*;
-
-    use std::collections::HashMap;
-    use std::io::Write;
-    use std::sync::Mutex;
-
     use lazy_static::lazy_static;
 
     // Mutex-protected HashMap for file handles
@@ -21,7 +19,7 @@ pub mod fasta_tools {
     pub fn get_filename(sequence_length: usize) -> String {
         let min_length = (sequence_length/1000)*1000;
         let max_length = min_length + 1000;
-    
+
         // pad with leading zeros so that we can concatenate the files together in order with cat later
         let min_length_padded = format!("{:0>12}", min_length);
         let max_length_padded = format!("{:0>12}", max_length);
@@ -32,7 +30,7 @@ pub mod fasta_tools {
         let sequence_length = record.seq().len();
         let filename = get_filename(sequence_length);
         let output_path = format!("{}/{}", output_directory, filename);
-    
+
         let mut handles = FILE_HANDLES.lock().unwrap(); // Lock the mutex here
         let file = handles.entry(output_path.clone()).or_insert_with(|| {
             OpenOptions::new()
@@ -42,7 +40,7 @@ pub mod fasta_tools {
                 .open(&output_path)
                 .expect("Error opening fasta file")
         });
-    
+
         let mut writer = fasta::Writer::new(file);
         writer.write_record(&record).expect("Error writing record");
     }
@@ -67,7 +65,7 @@ pub mod fasta_tools {
         while chunk.len() > 0 {
             chunk.par_iter().filter_map(|r| {
                 let rec = r.as_ref().expect("Error reading record");
-                process_seq_chunk_w_mutex(rec, &output_directory);
+                process_seq_chunk(rec, &output_directory);
                 Some(())
             }).collect::<Vec<_>>();
                 
@@ -86,3 +84,29 @@ pub mod fasta_tools {
     }
 }
 
+
+// use std::{fs, path::Path};
+// use tempfile::tempdir;
+
+// #[test]
+// fn test_break_up_fasta_by_sequence_lengthy() {
+//     // Setup
+//     let temp_dir = tempdir().unwrap();
+//     let test_dir_outputs = Path::new("test_data/fasta_tools/outputs");
+//     let fasta_file = Path::new("test_data/fasta_tools/inputs/nt");
+
+//     break_up_fasta_by_sequence_length(fasta_file, temp_dir.path(), 1000, 1000);
+
+//     // Compare files
+//     for entry in fs::read_dir(test_dir).unwrap() {
+//         let entry = entry.unwrap();
+//         let test_file_path = entry.path();
+//         let test_file_name = test_file_path.file_name().unwrap();
+//         let output_file_path = temp_dir.path().join(test_file_name);
+
+//         let test_data = fs::read(test_file_path).unwrap();
+//         let output_data = fs::read(output_file_path).unwrap();
+
+//         assert_eq!(test_data, output_data, "Files do not match: {:?}", test_file_name);
+//     }
+// }
