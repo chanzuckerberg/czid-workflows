9f0b8e044ba2c1724d8fcba194bcf8f7f0177a0f
lvreynoso <lreynoso@chanzuckerberg.com>
2024-01-19 12:26:04 -0800

    Add temp tag for test release

--- scripts/release.sh
+++ scripts/release.sh
@@ -28,6 +28,10 @@ else
     exit 1
 fi
 
+if [[ $WORKFLOW_NAME == amr ]]; then
+    TAG="amr-v1.3.2-patch-spadestrack"
+fi
+
 if [[ $( git branch --show-current) != "main" ]]; then 
     COMMIT=$(git rev-parse --short HEAD)
     TAG=$TAG"-$COMMIT"
